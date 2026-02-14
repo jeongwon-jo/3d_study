6afb9095e202403c732fb6a1ad7073e27fe19d61
jeongwon-jo <129711334+jeongwon-jo@users.noreply.github.com>
2026-02-14 20:36:34 +0900

    project 02 셋팅 및 chapter01 끝

---
+++ project02/example01/src/components/Earth.js
@@ -0,0 +1,11 @@
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import {Canvas, useLoader} from "@react-three/fiber"
+
+export function Earth(props) {
+  const glb = useLoader(GLTFLoader, "/models/earth.glb")
+  return (
+    <mesh {...props}>
+      <primitive object={glb.scene} />
+    </mesh>
+  )
+}
